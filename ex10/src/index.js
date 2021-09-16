1bad5ddcebbf9b6f2944d33a88d41e8e951ad864
Muhamed Bajramović <muhamed.bajramovicc@gmail.com>
2021-09-16 14:52:06 +0200

    Adding index.js file

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+  // only change code below this line
+  var myFirstName = "Muhamed";
+  var myLastName = "Bajramović";
+  var myHomeTown = "Cazin";
+  // only change code above this line
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+module.exports = main;
