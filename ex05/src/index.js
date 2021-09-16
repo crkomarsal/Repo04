7866a4caa6dd41ab5cfb234defd3acf2d446a1a2
Muhamed Bajramović <muhamed.bajramovicc@gmail.com>
2021-09-16 14:30:15 +0200

    Adding index.js file

---
+++ ex05/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+  var numOne;
+  var numTwo;
+  var product = numOne * numTwo;
+
+  // only change code below this line
+
+  // only change code above this line
+  return product;
+}
+
+console.log(main(2.5, 2.0)); //Change this line
+console.log(main(5.5, 5.5));
+console.log(main(100.25, 43.2));
+
+module.exports = main;
